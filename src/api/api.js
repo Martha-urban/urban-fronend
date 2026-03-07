25e369823f866f42482f50e882872341ca8aeda3
gitongasam <gitongas684@gmail.com>
2026-03-06 23:34:31 -0800

    fix vercel routing

--- src/api/api.js
+++ src/api/api.js
@@ -9,11 +9,17 @@ export const api = axios.create({
 
 console.log("API BASE URL:", import.meta.env.VITE_API_BASE_URL);
 
-// Attach token automatically
+// Attach token automatically (except for auth routes)
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("urban_access_token");
 
-  if (token) {
+  // Check if request is for login/register
+  const isAuthRequest =
+    config.url?.includes("/auth/login") ||
+    config.url?.includes("/auth/register");
+
+  // Attach token only for protected endpoints
+  if (token && !isAuthRequest) {
     config.headers.Authorization = `Bearer ${token}`;
   }
 
