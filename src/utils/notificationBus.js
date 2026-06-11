f4ce9fe5bbfb958965228eba41ac037672470a87
Martha-urban <marthawrites27@gmail.com>
2026-06-11 12:56:48 +0300

    add: notificationBus utility module

---
+++ src/utils/notificationBus.js
@@ -0,0 +1,13 @@
+const bus = new EventTarget();
+
+export function publishNotification(notification) {
+  bus.dispatchEvent(new CustomEvent("notification", { detail: notification }));
+}
+
+export function onNotification(handler) {
+  const wrapped = (e) => handler(e.detail);
+  bus.addEventListener("notification", wrapped);
+  return () => bus.removeEventListener("notification", wrapped);
+}
+
+export default { publishNotification, onNotification };
