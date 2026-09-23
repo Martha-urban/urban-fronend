ccbb53e7b1048a227896814cbda6bc06ee87f144
gitongasam <gitongas684@gmail.com>
2026-09-23 14:29:23 +0300

    Normalize API base URL paths

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL.replace(/\/api\/v1\/?$/, ""),
   headers: {
     "Content-Type": "application/json",
   },
